2d95b1b8ef53b7b432c87dfd58a5ec94e53a3781
CaioTakabatake <caio.soldera.taka@gmail.com>
2022-07-28 06:31:56 -0300

    utilizando regex para pegar links dentro do do arquivo lido

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ function throwError(err) {
     throw new Error(chalk.red(err.code, 'file not found'));
 }
 
+function getLinks(text) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const links = [];
+    let temp;
+
+    while ((temp = regex.exec(text)) !== null) {
+        links.push({ [temp[1]]: temp[2] });
+    }
+
+    return links;
+}
+
 async function getFile(path) {
     const encoding = 'utf-8';
 
     try {
         const text = await fs.promises.readFile(path, encoding);
-        console.log(chalk.green(text));
+        console.log(getLinks(text));
     } catch (err) {
         throwError(err);
     }
@@ -29,4 +41,4 @@ async function getFile(path) {
 //     });
 // }
 
-getFile('./arquivos/aa.md')
+getFile('./arquivos/texto1.md')
