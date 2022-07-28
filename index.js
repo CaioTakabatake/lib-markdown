3e25dce5977a3530357b205245bf6486d73f14ec
CaioTakabatake <caio.soldera.taka@gmail.com>
2022-07-28 04:48:44 -0300

    import with require

---
+++ index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello World!!!'))
+const paragrafo = 'Texto retornado por uma função';
+
+function texto(string) {
+    return string;
+}
+
+console.log(texto(paragrafo));
