3406b17254027147606f56c3b91b44fbf25f2c1d
CaioTakabatake <caio.soldera.taka@gmail.com>
2022-07-29 04:45:06 -0300

    validating urls

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const path = process.argv;
 async function processText(filePath) {
     const result = await getFile(filePath[2]);
     if (path[3] === 'validate') {
-        console.log(chalk.yellow('validated links'), validateURLs(result));
+        console.log(chalk.yellow('validated links'), await validateURLs(result));
     } else {
         console.log(chalk.yellow('link list'), result);
     }
--- http-validation.js
+++ http-validation.js
@@ -1,5 +1,22 @@
-function validateURLs(urlsArray) {
-    return urlsArray;
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function checkStatus(urlsArray) {
+    const statusArray = await Promise.all(urlsArray.map(async url => {
+        const res = await fetch(url);
+        return res.status
+    }));
+
+    return statusArray;
+}
+
+function generateURLsArray(urlsArray) {
+    return urlsArray.map(urlObject => Object.values(urlObject).join());
+}
+
+async function validateURLs(urlsArray) {
+    const links = generateURLsArray(urlsArray);
+    const statusLinks = await checkStatus(links);
+    return statusLinks;
 }
 
 module.exports = { validateURLs };
