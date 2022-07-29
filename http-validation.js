c95db46fa9d48be61912238fe9dbdd2acf3853c6
CaioTakabatake <caio.soldera.taka@gmail.com>
2022-07-29 05:03:24 -0300

    return object with url and status code

--- http-validation.js
+++ http-validation.js
@@ -1,12 +1,20 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+function handleErrors(err) {
+    throw new Error(err.message);
+}
 
 async function checkStatus(urlsArray) {
-    const statusArray = await Promise.all(urlsArray.map(async url => {
-        const res = await fetch(url);
-        return res.status
-    }));
-
-    return statusArray;
+    try {
+        const statusArray = await Promise.all(urlsArray.map(async url => {
+            const res = await fetch(url);
+            return res.status
+        }));
+    
+        return statusArray;
+    } catch (err) {
+        handleErrors(err);
+    }
 }
 
 function generateURLsArray(urlsArray) {
@@ -16,7 +24,11 @@ function generateURLsArray(urlsArray) {
 async function validateURLs(urlsArray) {
     const links = generateURLsArray(urlsArray);
     const statusLinks = await checkStatus(links);
-    return statusLinks;
+    const results = urlsArray.map((urlObject, index) => ({
+        ...urlObject, status: statusLinks[index]
+    }));
+
+    return results;
 }
 
 module.exports = { validateURLs };
