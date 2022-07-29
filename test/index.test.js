d02426f3d2f0e9d8dbfac0e8a24e3a0701c8c1ea
CaioTakabatake <caio.soldera.taka@gmail.com>
2022-07-29 05:50:29 -0300

    create test message when not has links

--- test/index.test.js
+++ test/index.test.js
@@ -8,8 +8,12 @@ describe('getFile::', () => {
     it('is a function?', () => {
         expect(typeof getFile).toBe('function');
     });
-    it('need reaturn array with results', async () => {
+    it('need return array with results', async () => {
         const result = await getFile('C:/Users/Caio/Documents/lib-markdown/test/arquivos/texto1.md');
         expect(result).toEqual(arrayResult);
+    });
+    it('need return message "links not found"', async () => {
+        const result = await getFile('C:/Users/Caio/Documents/lib-markdown/test/arquivos/texto1_no_links.md');
+        expect(result).toBe('links not found');
     });
 });
