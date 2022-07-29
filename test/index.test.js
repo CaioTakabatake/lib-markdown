8417ea951f861508ced167efd7d0aee543998a00
CaioTakabatake <caio.soldera.taka@gmail.com>
2022-07-29 05:43:46 -0300

    test getFile with jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,15 @@
 const { getFile } = require('../index');
 
-test('is a function?', () => {
-    expect(typeof getFile).toBe('function');
+const arrayResult = [{
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+}];
+
+describe('getFile::', () => {
+    it('is a function?', () => {
+        expect(typeof getFile).toBe('function');
+    });
+    it('need reaturn array with results', async () => {
+        const result = await getFile('C:/Users/Caio/Documents/lib-markdown/test/arquivos/texto1.md');
+        expect(result).toEqual(arrayResult);
+    });
 });
